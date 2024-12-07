d8453bc17e6444b7276eea83008a611e62a08eef
suryanig <67238881+suryanig@users.noreply.github.com>
2024-12-07 02:25:10 +0000

    continue with list story

--- lessons/02_Loops/04b_List_Story.py
+++ lessons/02_Loops/04b_List_Story.py
@@ -10,6 +10,25 @@ words = ['Once', '👦', 'upon', '🐕', 'park', 'met', 'with', 'a', 'the',
     'time', 'to', 'who', '🐈', '👧', 'and', 'went', 'had', 'play', '⚽.', 'they']
 
 story = []
+story.append(words[0:3:2])
+story.append(words[7:10:2])
+story.append(words[7])
+story.append(words[1])
+story.append(words[14])
+story.append(words[13])
+story.append(words[16])
+story.append(words[10])
+story.append(words[8])
+story.append(words[4])
+story.append(words[14])
+story.append(words[5])
+story.append(words[7])
+story.append(words[3])
+story.append(words[14])
+story.append(words[12])
+story.append(words[10])
+story.append(words[17])
+story.append(words[18])
 
 # Create a story using the words in the list
 
